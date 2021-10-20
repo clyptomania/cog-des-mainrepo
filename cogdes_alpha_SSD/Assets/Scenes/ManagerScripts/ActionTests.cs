7911b7356fdc6ec14b6dd0159087d24209ba8800
clyptomania <MaxSpur@users.noreply.github.com>
2021-10-20 17:27:21 +0200

    SteamVR Interaction
    Started updating controller interaction to SteamVR.

--- cogdes_alpha_SSD/Assets/Scenes/ManagerScripts/ActionTests.cs
+++ cogdes_alpha_SSD/Assets/Scenes/ManagerScripts/ActionTests.cs
@@ -5,13 +5,29 @@ using Valve.VR;
 
 public class ActionTests : MonoBehaviour {
 
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean testAction;
+
+
+
     // Start is called before the first frame update
     void Start () {
-
+        testAction.AddOnStateDownListener(TestDown, handType);
+        testAction.AddOnStateUpListener(TestUp, handType);
+        Debug.Log("Started ActionTests script");
     }
 
     // Update is called once per frame
     void Update () {
 
     }
+
+    public void TestUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource) {
+        Debug.Log("Action up!");
+    }
+    public void TestDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource) {
+        Debug.Log("Action down!");
+    }
+
+
 }
