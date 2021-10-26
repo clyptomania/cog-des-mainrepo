43dd8b8aaea229618741d4bcc4ac0292f9a59776
Maxim Spur <maxim.spur@icloud.com>
2021-10-26 16:03:54 +0200

    Questionnaire Slider
    Started developing the questionnaire functionality.

---
+++ cogdes_alpha_SSD/Assets/QuestionSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionSlider : MonoBehaviour {
+    public static QuestionSlider instance;
+    private Slider slider;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake() {
+        instance = this;
+
+        slider = GetComponentInChildren<Slider>();
+        Debug.Log("Slider found with value: " + slider.value);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
