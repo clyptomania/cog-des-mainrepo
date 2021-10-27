8ca554171d8052408278c2383f2e149c5b906bd4
RaoulBickmann <raoul@weltfern.com>
2021-10-27 15:08:49 +0200

    Update scene, add train movement/spawning functionality, add .obj files to git lfs

---
+++ cogdes_alpha_SSD/Assets/Scripts/TrainControl.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Trainstate;
+
+public class TrainControl : MonoBehaviour
+{
+
+    public Vector3 StopLocation { get; set; }
+    public Vector3 DespawnLocation { get; set; }
+    public int TotalStopTime { get; set; }
+    public int DoorDelay { get; set; }
+    public int TravelTime { get; set; }
+    public AnimationCurve PositionCurve { get; set; }
+    public TrainState State { get; set; }
+
+    private float secondsStationary;
+    private DoorControl[] doors;
+    private Vector3 startingPos;
+    private float currentTime;
+
+
+    void Start()
+    {
+        this.doors = this.GetComponentsInChildren<DoorControl>();
+        this.startingPos = this.transform.position;
+        this.currentTime = 0.0f;
+    }
+
+
+    void Update()
+    {
+        switch (this.State)
+        {
+            case TrainState.Arriving:
+                this.currentTime += Time.deltaTime;
+                var arrivalProgress = this.PositionCurve.Evaluate(this.currentTime / this.TravelTime);
+                this.transform.position = this.startingPos + (this.StopLocation - this.startingPos) * arrivalProgress;
+
+                if (Vector3.Distance(this.transform.position, this.StopLocation) < 0.001f)
+                {
+                    this.State = TrainState.DoorsOpening;
+
+                    //Open Doors
+                    this.StartCoroutine(this.ToggleDoors(this.DoorDelay, 0, TrainState.Waiting));
+                }
+
+                break;
+            case TrainState.DoorsOpening:
+                break;
+            case TrainState.Waiting:
+                if (this.secondsStationary >= (this.TotalStopTime - this.DoorDelay))
+                {
+                    this.State = TrainState.DoorsClosing;
+
+                    //Close Doors
+                    this.StartCoroutine(this.ToggleDoors(0, this.DoorDelay, TrainState.Departing));
+                }
+
+                this.secondsStationary += Time.deltaTime;
+                break;
+            case TrainState.DoorsClosing:
+                break;
+            case TrainState.Departing:
+                this.currentTime -= Time.deltaTime;
+                var departureProgress = this.PositionCurve.Evaluate(this.currentTime / this.TravelTime);
+                this.transform.position = this.DespawnLocation - (this.DespawnLocation - this.StopLocation) * departureProgress;
+
+                if (Vector3.Distance(this.transform.position, this.DespawnLocation) < 0.001f)
+                {
+                    Destroy(this.gameObject);
+                }
+
+                break;
+        }
+    }
+
+    IEnumerator ToggleDoors(int delayBefore, int delayAfter, TrainState nextState)
+    {
+        yield return new WaitForSeconds(delayBefore);
+
+        foreach (var door in this.doors)
+        {
+            door.ToggleDoors();
+        }
+
+        yield return new WaitForSeconds(delayAfter);
+        this.State = nextState;
+    }
+}
---
+++ cogdes_alpha_SSD/Assets/Scripts/TrainSpawner.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using static Trainstate;
+
+public class TrainSpawner : MonoBehaviour
+{
+    [Header("Spawner Settings")]
+    [SerializeField]
+    private int firstSpawnDelay;
+
+    [SerializeField]
+    private int spawnIntervall;
+
+    [SerializeField]
+    private bool spawnOnBEnabled;
+
+
+    [Header("Train Settings")]
+    [SerializeField]
+    private GameObject trainModel;
+
+    [SerializeField]
+    private Vector3 stopLocation;
+
+    [SerializeField]
+    private Vector3 despawnLocation;
+
+    [SerializeField]
+    private Vector3 spawnRotation;
+
+    [SerializeField]
+    private int totalStopTime;
+
+    [SerializeField]
+    private int travelTime;
+
+    [SerializeField]
+    private int doorDelay;
+
+    [SerializeField]
+    private AnimationCurve positionCurve;
+
+
+    void Start()
+    {
+        if(this.trainModel)
+        {
+            this.InvokeRepeating("SpawnTrain", this.firstSpawnDelay, this.spawnIntervall);
+        } else
+        {
+            Debug.Log("Train model missing.");
+        }
+    }
+
+    void SpawnTrain()
+    {
+        var train = Instantiate(this.trainModel, this.transform.position, Quaternion.Euler(this.spawnRotation.x, this.spawnRotation.y, this.spawnRotation.z));
+        var trainControl = train.GetComponent<TrainControl>();
+
+        if (trainControl)
+        {
+            trainControl.StopLocation = this.stopLocation;
+            trainControl.DespawnLocation = this.despawnLocation;
+            trainControl.TotalStopTime = this.totalStopTime;
+            trainControl.TravelTime = this.travelTime;
+            trainControl.DoorDelay = this.doorDelay;
+            trainControl.PositionCurve = this.positionCurve;
+            trainControl.State = TrainState.Arriving;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(this.spawnOnBEnabled && Input.GetKeyUp(KeyCode.B))
+        {
+            if (this.trainModel)
+            {
+                this.SpawnTrain();
+            }
+            else
+            {
+                Debug.Log("Train model missing.");
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (this.stopLocation != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(this.transform.position, this.stopLocation);
+        }
+
+        if (this.despawnLocation != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(this.stopLocation, this.despawnLocation);
+        }
+    }
+}
