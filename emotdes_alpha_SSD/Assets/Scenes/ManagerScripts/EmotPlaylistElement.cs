74c152d17efdc9b87aecb4395d05320207132242
MaxSpur <MaxSpur@users.noreply.github.com>
2021-11-02 17:56:30 +0100

    Annika: Cleanup Scenes, Scene 1_schwarzer Tunnel
    Cleanup unused old scenes/Folder (Scene 0.1 -0.5..., Scene zero)
    
    Neue Scene (Scene 1_schwarzer Tunnel)
    Neue Prefabs (Tunnelabschnitt_SetA_schwarz, Tunnelabschnitt_SetB_schwarz)

---
+++ emotdes_alpha_SSD/Assets/Scenes/ManagerScripts/EmotPlaylistElement.cs
@@ -0,0 +1,17 @@
+﻿
+public struct EmotPlaylistElement {
+
+    public EmotPlaylistElement(string roomName, int duration, int task_idx, int order_idx) {
+        this.roomName = roomName;
+        this.duration = duration;
+        this.task_idx = task_idx;
+        this.order_idx = order_idx;
+    }
+
+    public readonly string roomName;
+    public readonly int duration;
+    public readonly int task_idx;
+    public readonly int order_idx;
+
+    public string expName => $"{roomName}_{duration}_{task_idx}_{order_idx}";
+}
---
+++ emotdes_alpha_SSD/Assets/Scenes/ManagerScripts/SteamActions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SteamActions : MonoBehaviour {
+
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean testAction;
+
+
+
+    // Start is called before the first frame update
+    void Start () {
+        testAction.AddOnStateDownListener(TestDown, handType);
+        testAction.AddOnStateUpListener(TestUp, handType);
+        Debug.Log("Started ActionTests script");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void TestUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource) {
+        Debug.Log("Action up!");
+    }
+    public void TestDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource) {
+        Debug.Log("Action down!");
+    }
+
+
+}
